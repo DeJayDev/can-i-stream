b3ef127ca226cc96d4b7fcf1f41684a5813120df
DeJayDev <contact@dejaydev.com>
2022-03-18 15:06:52 -0500

    :paintbrush: Fix light theme

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function IndexRoute() {
     return (
         <div className="index flex flex-col h-full w-full justify-center items-center">
             <h1 className="font-sans text-4xl pb-3">Input an artists name...</h1>
-            <input type="text" className="rounded-md h-10 w-2/5 px-3.5 dark:text-black" />
+            <input type="text" className="rounded-md h-10 w-2/5 px-3.5 shadow-lg outline outline-2 outline-sky-400 dark:text-black" />
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //darkMode: 'class',
+  darkMode: 'class',
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
